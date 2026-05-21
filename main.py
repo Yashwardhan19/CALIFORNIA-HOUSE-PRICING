3daf7d6133d1381120977b24248536b85320dfa6
Yashwardhan Rathore <yashwardhan2027@gmail.com>
2026-05-21 19:42:26 +0530

    Trained best regression algorithm

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-# from sklearn.preprocessing import OrdinalEncoder  # Uncomment if you prefer ordinal
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+# from xgboost import XGBRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import root_mean_squared_error
  
 # 1. Load the data
 housing = pd.read_csv("housing.csv")
@@ -56,4 +63,80 @@ full_pipeline = ColumnTransformer([
 housing_prepared = full_pipeline.fit_transform(housing)
  
 # housing_prepared is now a NumPy array ready for training
-print(housing_prepared.shape)
+# print(housing_prepared.shape)
+
+# Linear Regression
+# lin_reg = LinearRegression()
+# lin_reg.fit(housing_prepared, housing_labels)
+ 
+# Decision Tree
+# tree_reg = DecisionTreeRegressor(random_state=42)
+# tree_reg.fit(housing_prepared, housing_labels)
+ 
+# Random Forest
+# forest_reg = RandomForestRegressor(random_state=42)
+# forest_reg.fit(housing_prepared, housing_labels)
+
+#Gradient Boosting Regressor
+gradient_reg = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1)
+gradient_reg.fit(housing_prepared, housing_labels)
+
+#XGBoost Regressor
+# xgb_reg = XGBRegressor(n_estimators=100, learning_rate=0.1)
+# xgb_reg.fit(housing_prepared, housing_labels)
+ 
+# Predict using training data
+# lin_preds = lin_reg.predict(housing_prepared)
+# tree_preds = tree_reg.predict(housing_prepared)
+# forest_preds = forest_reg.predict(housing_prepared)
+gradient_preds = gradient_reg.predict(housing_prepared)
+# xgb_preds = xgb_reg.predict(housing_prepared)
+ 
+# Calculate RMSE
+# lin_rmse = root_mean_squared_error(housing_labels, lin_preds)
+# tree_rmse = root_mean_squared_error(housing_labels, tree_preds)
+# forest_rmse = root_mean_squared_error(housing_labels, forest_preds)
+# gradient_rmse = root_mean_squared_error(housing_labels, gradient_preds)
+# xgb_rmse = root_mean_squared_error(housing_labels, forest_preds)
+ 
+# print("Linear Regression RMSE:", lin_rmse)
+# print("Decision Tree RMSE:", tree_rmse)
+# print("Random Forest RMSE:", forest_rmse)
+# print("Gradient Boosting RMSE:", gradient_rmse)
+# print("Random Forest RMSE:", xgb_rmse)
+
+#Output Rmse before cross validation
+'''
+Linear Regression RMSE: 69050.56219504567
+Decision Tree RMSE: 0.0
+Random Forest RMSE: 18342.366362322846
+Gradient Boosting RMSE: 53494.18776891836
+'''
+
+#Predicting rmses using Cross Validation
+# lin_rmses = -cross_val_score(lin_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+
+# tree_rmses = -cross_val_score(tree_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+
+# forest_rmses = -cross_val_score(forest_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+
+gradient_rmses = -cross_val_score(gradient_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+
+# print("Linear Regression RMSE:", lin_rmses)
+# print(pd.Series(lin_rmses).describe())
+# print("Decision Tree RMSE:", tree_rmses)
+# print(pd.Series(tree_rmses).describe())
+# print("Random Forest RMSE:", forest_rmses)
+# print(pd.Series(forest_rmses).describe())
+print("Gradient Boosting RMSE:", gradient_rmses)
+print(pd.Series(gradient_rmses).describe())
+# print("Random Forest RMSE:", xgb_rmses)
+
+
+#Output mean rmse using cross validation
+'''
+Linear Regression RMSE:  69204.322755
+Decision Tree RMSE:  69081.361563
+Random Forest RMSE: 49432.126788
+Gradient Boosting RMSE: 55427.942029
+'''
